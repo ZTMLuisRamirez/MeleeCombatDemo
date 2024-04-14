25eaab37360fcb96f33ca5117f1eeee51ae705ce
Luis R <jaskokoyn@gmail.com>
2024-04-14 19:33:12 -0400

    Fix projectile from hitting player twice POST

--- Source/MeleeCombatDemo/Private/Combat/EnemyProjectile.cpp
+++ Source/MeleeCombatDemo/Private/Combat/EnemyProjectile.cpp
@@ -5,28 +5,7 @@
 #include "GameFramework/Character.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Particles/ParticleSystemComponent.h"
-
-// Sets default values
-AEnemyProjectile::AEnemyProjectile()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void AEnemyProjectile::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AEnemyProjectile::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
+#include "Components/SphereComponent.h"
 
 void AEnemyProjectile::HandleBeginOverlap(AActor* OtherActor)
 {
@@ -46,6 +25,9 @@ void AEnemyProjectile::HandleBeginOverlap(AActor* OtherActor)
 		PawnRef->GetController(),
 		this
 	);
+
+	FindComponentByClass<USphereComponent>()
+		->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 	BeginExplosion();
 }
--- Source/MeleeCombatDemo/Public/Combat/EnemyProjectile.h
+++ Source/MeleeCombatDemo/Public/Combat/EnemyProjectile.h
@@ -10,15 +10,8 @@ UCLASS()
 class MELEECOMBATDEMO_API AEnemyProjectile : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AEnemyProjectile();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float damage{ 10.0f };
 
@@ -26,9 +19,6 @@ protected:
 	UParticleSystem* HitTemplate;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 	UFUNCTION(BlueprintCallable)
 	void HandleBeginOverlap(AActor* OtherActor);
 
